a15abf8b44cd80a803ec8ad337c679e23950c21b
Razak <moabdulrazak17@gmail.com>
2023-05-29 22:12:15 +0000

    feat: create form-input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { FormInput } from "@/components/form-input";
 import { Header } from "@/components/header";
 
 export default function Home() {
@@ -5,6 +6,10 @@ export default function Home() {
     <div className="p-20 h-full">
       <div className="h-full flex flex-col">
         <Header />
+        <div className="flex-1 overflow-y-auto">
+          {/* TODO: Response item  */}
+        </div>
+        <FormInput />
       </div>
     </div>
   );
---
+++ components/form-input.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+export function FormInput() {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
+  return (
+    <div
+      className="
+        py-4
+        px-4 
+        bg-white 
+        border
+        flex 
+        items-center 
+        gap-2 
+        lg:gap-4 
+        w-full
+      "
+    >
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center gap-2 lg:gap-4 w-full"
+      >
+        <div className="relative w-full">
+          <input
+            id="message"
+            required
+            placeholder="Write a message"
+            className="
+          text-black
+          font-light
+          py-4
+          px-4
+          bg-neutral-100 
+          w-full 
+          focus:outline-none
+        "
+          />
+        </div>
+        <button
+          type="submit"
+          className="
+            px-6 
+            py-4 
+            bg-sky-500 
+            cursor-pointer 
+            hover:bg-sky-600 
+            transition
+          "
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
